66657379f675bae43b8f593123f19dc6bce7fff7
Anthony <AnthonySgro1995@gmail.com>
2021-04-26 16:51:37 -0400

    added ssl error handling

--- server/db/db.js
+++ server/db/db.js
@@ -1,7 +1,18 @@
 const { Sequelize } = require("sequelize");
+const config = { logging: false };
+
+// Handles the error that prof discusses in this video: https://youtu.be/lwAJPU23kok?t=2477
+if (process.env.SSL) {
+    config.dialectOptions = {
+        ssl: {
+            rejectUnauthorized: false,
+        },
+    };
+}
+
 const db = new Sequelize(
     process.env.DATABASE_URL || "postgres://localhost:5432/graceShopper",
-    { logging: false },
+    config,
 );
 
 module.exports = db;
